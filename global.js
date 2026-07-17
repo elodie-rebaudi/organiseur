98762d1c776d8a202f58bb3655de1adae88d9a4a
elodie-rebaudi <elodie.rebaudi@etu.univ-cotedazur.fr>
2026-07-17 14:49:25 +0200

    refactor: create dynamic addButton function in global.js

--- global.js
+++ global.js
@@ -1,52 +1,11 @@
 document.addEventListener("DOMContentLoaded", () =>{
 
-    /* home Button*/
-    const homeButton = document.createElement("a");
-    homeButton.href = "index.html";
-    homeButton.className = "home-btn";
-
-    const homeIcon = document.createElement("img");
-    homeIcon.src = "icons/homebutton.png";
-    homeIcon.alt = "Home icon";
-    homeIcon.className = "card-icon";
-
-    homeButton.appendChild(homeIcon);
-
-    /*planner button*/
-    const plannerButton = document.createElement("a");
-    plannerButton.href = "planner.html";
-    plannerButton.className = "planner-btn";
-
-    const plannerIcon = document.createElement("img");
-    plannerIcon.src = "icons/planner.png";
-    plannerIcon.alt = "planner icon";
-    plannerIcon.className = "card-icon";
-
-    plannerButton.appendChild(plannerIcon);
-
-    /*todo button*/
-    const todoButton = document.createElement("a");
-    todoButton.href = "todo.html";
-    todoButton.className = "todo-btn";
-
-    const todoIcon = document.createElement("img");
-    todoIcon.src = "icons/todo.png";
-    todoIcon.alt = "todo icon";
-    todoIcon.className = "card-icon";
-
-    todoButton.appendChild(todoIcon);
-
-    /*cookbook button*/
-    const cookbookButton = document.createElement("a");
-    cookbookButton.href = "cookbook.html";
-    cookbookButton.className = "cookbook-btn";
-
-    const cookbookIcon = document.createElement("img");
-    cookbookIcon.src = "icons/cookbook.png";
-    cookbookIcon.alt = "cookbook icon";
-    cookbookIcon.className = "card-icon";   
-
-    cookbookButton.appendChild(cookbookIcon);
+    const homeButton = addButton("home");
+    const plannerButton = addButton("planner");
+    const todoButton = addButton("todo");
+    const cookbookButton = addButton("cookbook");
+    const videogamesButton = addButton("videogames");
+    const tvshowsButton = addButton("tvshows");
 
     const navMenu = document.createElement("div");
     navMenu.className = "global-nav-menu";
@@ -55,6 +14,27 @@ document.addEventListener("DOMContentLoaded", () =>{
     navMenu.appendChild(plannerButton);
     navMenu.appendChild(todoButton);
     navMenu.appendChild(cookbookButton);
+    navMenu.appendChild(videogamesButton);
+    navMenu.appendChild(tvshowsButton);
 
     document.body.prepend(navMenu);
 });
+
+function addButton(name){
+    btn = document.createElement("a");
+    if (name=="home"){
+        btn.href = "index.html";
+    } else {
+        btn.href = `${name}.html`;
+    }
+    btn.className = `${name}-btn`;
+
+    const icon = document.createElement("img");
+    icon.src = `icons/${name}.png`;
+    icon.alt = `${name} icon`;
+    icon.className = "card-icon";   
+
+    btn.appendChild(icon);
+
+    return btn;
+};
